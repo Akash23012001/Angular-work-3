e3c376e753a412d9cc2bcea0f2e03ea30d7bac78
Akash Dhananjay Prayag <91182131+Akash23012001@users.noreply.github.com>
2023-01-15 13:01:20 +0530

    file added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenderPipe } from './custom_pipes/gender.pipe';
+import { SquarePipe } from './custom_pipes/square.pipe';
+import { CircleAreaPipe } from './custom_pipes/circle-area.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenderPipe,
+    SquarePipe,
+    CircleAreaPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
